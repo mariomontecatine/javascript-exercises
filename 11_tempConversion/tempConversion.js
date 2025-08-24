7fcc051810becd0531d769ef0a34dac641c9cc08
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-24 13:51:59 +0200

    Exercise 11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  
+  let newTemp = ((temp - 32) * (5 / 9));
+  newTemp = Math.round(newTemp * 10) / 10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = (temp * (9 / 5) + 32);
+  newTemp = Math.round(newTemp * 10) / 10;
+  return newTemp;
 };
 
 // Do not edit below this line
