4fcef197317b7651b6d2ce2809a94741ad1d6ad3
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-24 13:31:27 +0200

    Exercise 09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let total = 0;
+    let smallNum = (a < b) ? a : b;
+    let bigNum = (a < b) ? b : a;
+    
 
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) ) {
+        return "ERROR"
+    }
+
+    for (let i = smallNum; i <= bigNum; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
