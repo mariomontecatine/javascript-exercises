6cdf109ce105fd27998955eb2bec15cd77eeea13
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-24 12:09:49 +0200

    Exercise 06 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0) {
+        let repetition = ""
+        return repetition;
+    } else if (num < 0) {
+        let repetition = "ERROR";
+        return repetition;        
+    } 
+    else {
+    let repetition = string;
+    for (let i = 1; i < num ; i++) {
+        repetition += string;
+    }
+    return repetition;
+}
 };
 
 // Do not edit below this line
