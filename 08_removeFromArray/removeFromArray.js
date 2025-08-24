44b02cb170edebd2e1732190f0adb34c79a98734
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-24 12:40:33 +0200

    Exercise 08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...num) {
+    let removed = array;
+    for (let i = 0; i < num.length; i++) {
+        removed = removed.filter(item => item !== num[i]);
+        
+    }
+    return removed;
 };
 
 // Do not edit below this line
